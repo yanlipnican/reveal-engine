265b6e284418c48620200ae494f4b11268be599f
Jan Lipnican <jan.lipnican@cognito.cz>
2017-07-21 22:29:12 +0200

    install dependencies shell script

--- run.sh
+++ run.sh
@@ -5,8 +5,20 @@ BIN_NAME="engine"
 DIR_NAME=`readlink -f "$0"`
 DIR_NAME=${DIR_NAME%/*}
 
+download_file() {
+    FILENAME=${DIR_NAME}/external/$2
+    rm -rf $FILENAME $FILENAME.zip
+    wget -O $FILENAME.zip $1
+    unzip $FILENAME.zip -d ${DIR_NAME}/external
+    rm -rf $FILENAME.zip
+}
+
 if [ $1 = "prepare" ]; then
     cmake $DIR_NAME
+elif [ $1 = "deps" ]; then
+    download_file "https://github.com/glfw/glfw/releases/download/3.2.1/glfw-3.2.1.zip" "glfw-3.2.1"
+    download_file "https://sourceforge.net/projects/glew/files/glew/2.0.0/glew-2.0.0.zip" "glew-2.0.0"
+    download_file "https://github.com/g-truc/glm/releases/download/0.9.8.4/glm-0.9.8.4.zip" "glm"
 elif [ $1 = "build" ]; then
     make -C $DIR_NAME
 elif [ $1 = "valgrind" ]; then
@@ -14,3 +26,5 @@ elif [ $1 = "valgrind" ]; then
 elif [ $1 = "start" ]; then
     ${DIR_NAME}/${BIN_DIR}/${BIN_NAME}
 fi
+
+echo "Done"
